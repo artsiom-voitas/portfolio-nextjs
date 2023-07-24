c1c19eff4591c6dbed6fd0da3c00c5b6c472892f
Artsiom Voitas <artsiom.voitas@gmail.com>
2023-07-25 00:11:17 +0300

    refactor: changes in images imports
    changes in images imports, added index.d.ts to gitignore for better vscode experience without displaying ts errors

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,14 +1,17 @@
-import avStore from '@/assets/images/projects/av-store.png';
-import githubDashboard from '@/assets/images/projects/github-dashboard.png';
-import honeyStore from '@/assets/images/projects/honey-store.png';
-import mortgageCalc from '@/assets/images/projects/mortgage-calc.png';
-import rayalPark from '@/assets/images/projects/rayal-park.png';
-import todoApp from '@/assets/images/projects/todo-app.png';
-import universalStore from '@/assets/images/projects/universal-store.png';
-import webovio from '@/assets/images/projects/webovio.png';
 import AnimatedText from '@/components/AnimatedText';
 import Layout from '@/components/Layout';
-import { FeaturedProject, Project } from '@/components/Projects';
+import {
+    FeaturedProject,
+    Project,
+    avStore,
+    githubDashboard,
+    honeyStore,
+    mortgageCalc,
+    rayalPark,
+    todoApp,
+    universalStore,
+    webovio
+} from '@/components/Projects';
 import Head from 'next/head';
 
 function Projects() {
