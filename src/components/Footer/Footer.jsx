ffd9ebe669f129a0c55ea1ca89e90d8b5f6fd5da
Artsiom Voitas <artsiom.voitas@gmail.com>
2023-07-26 00:28:35 +0300

    feat: added responsive design to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ function Footer() {
     return (
         <footer
             className="w-full border-t-2 border-solid border-dark text-lg
-                font-medium dark:border-light dark:text-light">
-            <Layout className="mt-2 flex items-center justify-between !py-8">
+                font-medium dark:border-light dark:text-light sm:text-base">
+            <Layout className="mt-2 flex items-center justify-between !py-8 lg:flex-col lg:py-6">
                 <span>{currentYear} &copy; All Rights Reserved.</span>
-                <div className="flex items-center">
+                <div className="flex items-center lg:py-2">
                     Build With
                     <Link
                         className="ml-2 rounded-lg border border-solid border-black bg-black
