77e966807ef0be5a6e463f8929aab689345c3c4d
Artsiom Voitas <artsiom.voitas@gmail.com>
2023-07-25 22:45:38 +0300

    feat: added responsive design to layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({ children, className = '' }) {
     return (
         <div
             className={`${className} z-0 inline-block h-full w-full
-                    bg-light p-32 pt-0 dark:bg-dark`}>
+                    bg-light p-32 pt-0 dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p-8`}>
             {children}
         </div>
     );
